8bf2224197a28fbfba3c5beecac2416495998821
Lucas Pereira <lucasp28.contato@gmail.com>
2020-07-31 16:28:30 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,9 @@
 import app from './config/app'
+import env from './config/env'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 
-app.listen(3333, () => console.log('Server running at http://localhost:3333'))
+MongoHelper.connect(env.mongoUrl)
+  .then(() => {
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
+  .catch(e => console.error(e))
