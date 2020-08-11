f893405baafe2d3448d2180dc87e133e4ce761a5
Lucas Pereira <lucasp28.contato@gmail.com>
2020-08-11 16:17:19 -0300

    refactor: move password comparation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -2,6 +2,7 @@ import { makeSignUpValidation } from './signup-validations'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { RequiredFieldsValidation } from '../../presentation/helpers/validators/required-fields-validation'
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 
 jest.mock('../../presentation/helpers/validators/validation-composite')
 
@@ -13,6 +14,7 @@ describe('SignUpValidation Factory', () => {
     fields.forEach(field => {
       validations.push(new RequiredFieldsValidation(field))
     })
+    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
--- src/main/factories/signup-validations.ts
+++ src/main/factories/signup-validations.ts
@@ -1,6 +1,7 @@
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { RequiredFieldsValidation } from '../../presentation/helpers/validators/required-fields-validation'
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 
 export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = []
@@ -8,5 +9,6 @@ export const makeSignUpValidation = (): ValidationComposite => {
   fields.forEach(field => {
     validations.push(new RequiredFieldsValidation(field))
   })
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
   return new ValidationComposite(validations)
 }
