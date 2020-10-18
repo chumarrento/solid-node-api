cdab4d0461dcbdb9bfdd381d0c93da9863c4ca3e
Lucas Pereira <lucasp28.contato@gmail.com>
2020-10-18 15:51:03 -0300

    feat: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,10 +1,15 @@
-import { AccessDeniedError } from '../errors'
-import { forbidden } from '../helpers/http/http-helper'
-import { HttpRequest, HttpResponse, Middleware } from '../protocols'
+import { LoadAccountByToken, AccessDeniedError, forbidden, HttpResponse, HttpRequest, Middleware } from './auth-middleware-protocols'
 
 export class AuthMiddleware implements Middleware {
+  constructor (
+    private readonly loadAccountByToken: LoadAccountByToken
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = forbidden(new AccessDeniedError())
-    return new Promise(resolve => resolve(error))
+    const accessToken = httpRequest.headers?.['x-access-token']
+    if (!accessToken) {
+      return forbidden(new AccessDeniedError())
+    }
+    await this.loadAccountByToken.load(accessToken)
   }
 }
