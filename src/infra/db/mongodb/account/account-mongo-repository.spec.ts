aba20063415baafc747100c363c132946a6c8649
Lucas Pereira <lucasp28.contato@gmail.com>
2020-10-20 18:52:21 -0300

    test: ensure AccountMongoRepository find an account by token with role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -92,5 +92,22 @@ describe('Account Mongo Repository', () => {
       expect(account.email).toEqual('any_email@email.com')
       expect(account.password).toEqual('any_password')
     })
+
+    test('Should return an account on loadByEmail with role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        accessToken: 'any_token',
+        role: 'any_role'
+      })
+      const account = await sut.loadByToken('any_token', 'any_role')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toEqual('any_name')
+      expect(account.email).toEqual('any_email@email.com')
+      expect(account.password).toEqual('any_password')
+    })
   })
 })
