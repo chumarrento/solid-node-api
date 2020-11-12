c269e99492e11c5997f3bc37660dc639e9e8c4e2
Lucas Pereira <lucasp28.contato@gmail.com>
2020-11-12 15:38:16 -0300

    refactor: move mocks from presentation to unique file

--- src/presentation/test/mock-account.ts
+++ src/presentation/test/mock-account.ts
@@ -1,6 +1,7 @@
 import { mockAccountModel } from '@/domain/test'
 import { AccountModel } from '@/domain/models/account'
 import { AddAccount, AddAccountParams } from '@/domain/usecases/account/add-account'
+import { LoadAccountByToken } from '@/domain/usecases/account/load-account-by-token'
 
 export const mockAddAccount = (): AddAccount => {
   class AddAccountStub implements AddAccount {
@@ -9,4 +10,14 @@ export const mockAddAccount = (): AddAccount => {
     }
   }
   return new AddAccountStub()
+}
+
+export const mockLoadAccountByToken = (): LoadAccountByToken => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load (accessToken: string, role?: string): Promise<AccountModel> {
+      return new Promise(resolve => resolve(mockAccountModel()))
+    }
+  }
+
+  return new LoadAccountByTokenStub()
 }
