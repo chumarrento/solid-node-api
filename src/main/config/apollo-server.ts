ca8048f58f7589b91259fdb4d75929182070a784
Lucas Pereira <lucasp28.contato@gmail.com>
2020-12-29 21:26:28 -0300

    feat: add status code error on exceptions

--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -3,11 +3,56 @@ import resolvers from '@/main/graphql/resolvers'
 
 import { ApolloServer } from 'apollo-server-express'
 import { Express } from 'express'
+import { GraphQLError } from 'graphql'
+
+type GraqphQLErrorsTypes = {
+  name: string
+  status: number
+}
+
+const httpErrors: GraqphQLErrorsTypes[] = [
+  {
+    name: 'UserInputError',
+    status: 400
+  },
+  {
+    name: 'AuthenticationError',
+    status: 401
+  },
+  {
+    name: 'ForbiddenError',
+    status: 403
+  },
+  {
+    name: 'ApolloError',
+    status: 500
+  }
+]
+
+const handleErrors = (response: any, errors: readonly GraphQLError[]): void => {
+  errors?.forEach(error => {
+    response.data = undefined
+    httpErrors.forEach(httpError => {
+      if (checkError(error, httpError.name)) {
+        response.http.status = httpError.status
+      }
+    })
+  })
+}
+
+const checkError = (error: GraphQLError, errorName: string): boolean => {
+  return [error.name, error.originalError?.name].some(name => name === errorName)
+}
 
 export default (app: Express): void => {
   const server = new ApolloServer({
     resolvers,
-    typeDefs
+    typeDefs,
+    plugins: [{
+      requestDidStart: () => ({
+        willSendResponse: ({ response, errors }) => handleErrors(response, errors)
+      })
+    }]
   })
   server.applyMiddleware({ app })
 }
